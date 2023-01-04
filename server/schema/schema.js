58de562acd3ea8d19aaf560d90df5ce4ae064bb8
Rahul Kumar <rahulrai2822march@gmail.com>
2023-01-04 20:56:34 +0530

    schema updated

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -43,7 +43,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -58,7 +58,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, { authorId: parent.id });
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -71,30 +71,35 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       // RESOLVE FUNCTION FETCHS DATA FROM THE DATABASE AS PER THE QUERY
-      // resolve(parent, args) {
-      //   // args.id?
-      //   return _.find(books, { id: args.id });
-      // },
+      resolve(parent, args) {
+        return Book.findById(args.id);
+
+        // args.id?
+        // return _.find(books, { id: args.id });
+      },
     },
     books: {
       type: new GraphQLList(BookType),
-      // resolve(parent, args) {
-      //   return books;
-      // },
+      resolve(parent, args) {
+        return Book.find({})
+      },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
-      // resolve(parent, args) {
-      //   return _.find(authors, { id: args.id });
-      // },
+      resolve(parent, args) {
+        return Author.findById(args.id);
+        // return _.find(authors, { id: args.id });
+      },
     },
 
     authors: {
       type: new GraphQLList(AuthorType),
-      // resolve(parent, args) {
-      //   return authors;
-      // },
+      resolve(parent, args) {
+
+        return Author.find({})
+        // return authors;
+      },
     },
   },
 });
